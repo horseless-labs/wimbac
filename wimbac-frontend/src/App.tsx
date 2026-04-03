a3cadbade36ccc625591c585717d86ca3bba4911
mireles <mireles@horseless.io>
2026-04-03 13:09:19 -0400

    Preliminary commit of frontend modernization

---
+++ wimbac-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <h1>WIMBAC Modern</h1>
+      <div id="map-placeholder" style={{ background: '#ddd', height: '500px' }}>
+        Map will go here soon.
+      </div>
+    </div>
+  )
+}
+
+export default App
